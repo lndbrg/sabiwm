2725d701f2b7aaa7d93cb01fef68f4c165eb94bb
Simon Wollwage <mail.wollwage@gmail.com>
2016-12-19 13:32:47 +0900

    feat: add error-chain and log initialization

--- src/bin/sabiwm.rs
+++ src/bin/sabiwm.rs
@@ -1,5 +1,18 @@
 extern crate sabiwm;
+#[macro_use(o, slog_log, slog_error)]
+extern crate slog;
+#[macro_use]
+extern crate slog_scope;
 
 fn main() {
-    ::sabiwm::initialize();
+    if let Err(ref e) = ::sabiwm::run() {
+        error!("{}", e);
+        for e in e.iter().skip(1) {
+            error!("caused by: {}", e);
+        }
+        if let Some(backtrace) = e.backtrace() {
+            error!("backtrace: {:?}", backtrace);
+        }
+        std::process::exit(1);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,15 @@ extern crate slog_scope;
 extern crate slog_stream;
 extern crate xcb;
 extern crate xdg;
+#[macro_use]
+extern crate error_chain;
 
 mod core;
+mod errors {
+    error_chain!{}
+}
 
+use errors::*;
 use std::ffi::OsString;
 use std::env::var_os;
 use std::fs::File;
@@ -17,15 +23,24 @@ use slog_stream::stream;
 use slog_scope::set_global_logger;
 use xdg::BaseDirectories;
 
-pub fn initialize() {
-    let xdg = BaseDirectories::with_prefix("sabiwm").unwrap();
-    let path = xdg.place_cache_file("sabiwm.log").unwrap();
-    let file = File::create(path).unwrap();
+pub fn run() -> Result<()> {
+    initialize_logger().chain_err(|| "unable to initialize logger")
+}
+
+pub fn initialize_logger() -> Result<()> {
+    let xdg =
+        BaseDirectories::with_prefix("sabiwm").chain_err(|| "unable to get xdg base directory")?;
+    let path = xdg.place_cache_file("sabiwm.log")
+        .chain_err(|| "unable to get path for log file")?;
+    let file = File::create(path).chain_err(|| "unable to create log file")?;
     let file_logger = stream(file, ::slog_json::new().add_default_keys().build());
     let logger = Logger::root(file_logger.ignore_err(),
                               o!("sabiwm" => env!("CARGO_PKG_VERSION")));
 
     set_global_logger(logger);
 
-    info!("initializing sabiwm");
+    info!("initialized logger");
+    info!("sabiwm version {}", env!("CARGO_PKG_VERSION"));
+
+    Ok(())
 }
